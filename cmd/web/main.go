c5e3e900e6c3b5d43de0f51294284ec6817c8a77
Stephen Donovan <steviedons@gmail.com>
2023-11-09 16:29:53 +0000

    chapter 3 finished

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,6 +14,9 @@ type application struct {
 
 func main() {
 
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
@@ -22,19 +25,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	addr := flag.String("addr", ":8080", "HTTP network address")
-	flag.Parse()
-
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
@@ -42,7 +32,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on port: %s", *addr)
